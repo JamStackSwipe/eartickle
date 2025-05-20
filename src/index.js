572e74c9ab2995373008a96a03b09e127ddae1aa
JamStackSwipe <eric@hochacool.com>
2025-05-19 23:33:41 -0500

    Update index.js
    not much changed here

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "./App"; // Must match case
+
+import "./index.css"; // optional, if using Tailwind
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
