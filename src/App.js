7c76df2c9625dd2ab5e5b662abff635bad2e94a4
JamStackSwipe <eric@hochacool.com>
2025-05-20 00:23:10 -0500

    Update App.js
    Routers Support

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
+
 import AuthScreen from "./screens/AuthScreen";
 import SwipeScreen from "./screens/SwipeScreen";
 import JamStackView from "./screens/JamStackView";
@@ -7,15 +9,21 @@ import UploadScreen from "./screens/UploadScreen";
 
 function App() {
   return (
-    <div className="bg-black min-h-screen text-white">
-      <Header />
-      <main className="p-6 space-y-12">
-        <AuthScreen />
-        <SwipeScreen />
-        <JamStackView />
-        <UploadScreen />
-      </main>
-    </div>
+    <Router>
+      <div className="bg-black min-h-screen text-white">
+        <Header />
+        <main className="p-6">
+          <Routes>
+            <Route path="/" element={<Navigate to="/swipe" />} />
+            <Route path="/auth" element={<AuthScreen />} />
+            <Route path="/swipe" element={<SwipeScreen />} />
+            <Route path="/jamstack" element={<JamStackView />} />
+            <Route path="/upload" element={<UploadScreen />} />
+            <Route path="*" element={<p className="text-red-500">404 - Not Found</p>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
