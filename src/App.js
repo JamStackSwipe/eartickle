f1f4c8ecb22dc12d4cb3904d94bf655127451dba
JamStackSwipe <eric@hochacool.com>
2025-05-19 22:07:54 -0500

    Add files via upload
    This has the router so it should work better now.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthScreen from "./screens/AuthScreen";
-import SwipeScreen from "./screens/SwipeScreen";
+import Dashboard from "./screens/Dashboard";
 import JamStackView from "./screens/JamStackView";
+import SwipeScreen from "./screens/SwipeScreen";
 import UploadScreen from "./screens/UploadScreen";
 
 function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1 style={{ color: "#fff" }}>EarTickle</h1>
-      <AuthScreen />
-      <hr />
-      <SwipeScreen />
-      <hr />
-      <JamStackView />
-      <hr />
-      <UploadScreen />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SwipeScreen />} />
+        <Route path="/auth" element={<AuthScreen />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/jamstack" element={<JamStackView />} />
+        <Route path="/upload" element={<UploadScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
