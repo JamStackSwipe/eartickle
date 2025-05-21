9e3b9ce71a342137be0d239351392bb70e0035a9
JamStackSwipe <eric@hochacool.com>
2025-05-21 09:32:17 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../supabase';
 
 const Header = () => {
   const [user, setUser] = useState(null);
