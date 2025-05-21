aa4539ae5f7f4e02bd6ef6abb5152097ca0133e1
JamStackSwipe <eric@hochacool.com>
2025-05-20 21:18:16 -0500

    Add files via upload
    Trying to restore to older styled versions. Everything is testing ok

---
+++ src/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
+// Screen imports
+import LoginScreen from './screens/LoginScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import UploadScreen from './screens/UploadScreen';
+import SwipeScreen from './screens/SwipeScreen';
+import JamStackScreen from './screens/JamStackScreen';
+import AuthScreen from './screens/AuthScreen';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginScreen />} /> {/* Default to login */}
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/upload" element={<UploadScreen />} />
+        <Route path="/swipe" element={<SwipeScreen />} />
+        <Route path="/jamstack" element={<JamStackScreen />} />
+        <Route path="/auth" element={<AuthScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
