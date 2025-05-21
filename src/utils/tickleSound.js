536e9f6f517318e529649cbb95f7e51c0d6cd324
JamStackSwipe <eric@hochacool.com>
2025-05-21 01:27:25 -0500

    Add files via upload
    Tickle Engine

---
+++ src/utils/tickleSound.js
@@ -0,0 +1,21 @@
+// src/utils/tickleSound.js
+
+export const playTickle = () => {
+  const audio = new Audio('/sounds/tickle.mp3');
+  audio.play();
+};
+
+export const playTickleSent = () => {
+  const audio = new Audio('/sounds/tickle-sent.mp3');
+  audio.play();
+};
+
+export const playTickleWelcome = () => {
+  const audio = new Audio('/sounds/tickle-welcome.mp3');
+  audio.play();
+};
+
+export const playTickleSpecial = () => {
+  const audio = new Audio('/sounds/tickle-special.mp3');
+  audio.play();
+};
