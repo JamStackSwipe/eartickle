9d1ea68677fe88a63455dfd361fd5f9a55389b66
JamStackSwipe <eric@hochacool.com>
2025-05-24 00:03:29 -0500

    Update Header.js
    Added our Tagline... Swipe, Stack, Play

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,12 +35,12 @@ const Header = () => {
 
   return (
     <header className="w-full px-4 py-3 bg-black text-white flex justify-between items-center shadow relative z-10">
-      <div className="flex items-center space-x-4">
-        <div
-          onClick={handleLogoClick}
-          className="text-xl font-bold text-white hover:text-gray-300 cursor-pointer"
-        >
+      <div className="flex flex-col cursor-pointer" onClick={handleLogoClick}>
+        <div className="text-xl font-bold text-white hover:text-gray-300">
           🎵 EarTickle
+        </div>
+        <div className="text-xs text-gray-400 -mt-1 ml-1">
+          Swipe. Stack. Play.
         </div>
       </div>
 
@@ -58,30 +58,63 @@ const Header = () => {
           <Link to="/stacker" className="hover:underline text-white">
             🎶 Stacker
           </Link>
+          <Link to={`/artist/${user.id}`} className="hover:underline text-white">
+            My Artist Page
+          </Link>
 
-          {/* Avatar + Dropdown */}
+          {/* Avatar + Dropdown Menu */}
           <div
             className="relative"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
-            <Link to="/profile">
-              <img
-                src={user.user_metadata?.avatar_url || '/default-avatar.png'}
-                alt="avatar"
-                className="w-8 h-8 rounded-full border border-white cursor-pointer"
-              />
-            </Link>
+            <img
+              src={user.user_metadata?.avatar_url || '/default-avatar.png'}
+              alt="avatar"
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="w-8 h-8 rounded-full border border-white cursor-pointer"
+            />
 
             {menuOpen && (
-              <div className="absolute right-0 mt-2 w-32 bg-white text-black rounded shadow-lg text-sm overflow-hidden">
-                <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100">
+              <div
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className="absolute right-0 mt-2 w-40 bg-white text-black rounded shadow-lg text-sm overflow-hidden"
+              >
+                <Link
+                  to="/settings"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
                   ⚙️ Settings
                 </Link>
-                {/* Uncomment below when is_artist logic is ready */}
-                {/* <Link to={`/artist/${user.id}`} className="block px-4 py-2 hover:bg-gray-100">
-                  🎤 My Artist Page
-                </Link> */}
+                <Link
+                  to="/privacy"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
+                  🔒 Privacy
+                </Link>
+                <Link
+                  to="/terms"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
+                  📄 Terms
+                </Link>
+                <a
+                  href="https://github.com/JamStackSwipe/eartickle"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
+                  🔗 GitHub
+                </a>
+                <a
+                  href="https://github.com/JamStackSwipe/eartickle/wiki"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                >
+                  📘 Wiki
+                </a>
                 <button
                   onClick={handleLogout}
                   className="w-full text-left px-4 py-2 hover:bg-gray-100"
