bf50f7e25cc02dc6c8a5d6239c3ebd5a9eadade8
JamStackSwipe <eric@hochacool.com>
2025-05-23 20:49:40 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,4 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  global: {
+    headers: {
+      'Accept': 'application/json', // ✅ Fixes 406 error
+    },
+  },
+});
