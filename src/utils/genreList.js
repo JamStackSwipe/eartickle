4472d95d7760a0ea74827ae6731bced7040c1d6b
JamStackSwipe <eric@hochacool.com>
2025-05-26 23:34:08 -0500

    Create genreList.js

---
+++ src/utils/genreList.js
@@ -0,0 +1,16 @@
+// src/utils/genreList.js
+export const genreOptions = [
+  'pop',
+  'rock',
+  'hiphop',
+  'country',
+  'worship',
+  'lofi',
+  'electronic',
+  'comedy',
+  'ambient',
+  'indie',
+  'instrumental',
+  'spokenword',
+  'other',
+];
