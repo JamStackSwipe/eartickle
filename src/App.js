4502f8f9949788d7fc5127f55338364a5df81b71
JamStackSwipe <eric@hochacool.com>
2025-06-02 19:32:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import FooterGlobal from './components/FooterGlobal';
