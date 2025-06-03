76839cbeaa26ac85a0eeda27c07fcba53b62f88b
JamStackSwipe <eric@hochacool.com>
2025-06-03 04:32:01 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  safelist: [
+    'bg-amber-600',
+    'bg-blue-600',
+    'bg-pink-600',
+    'bg-purple-600',
+    'bg-cyan-600',
+    'bg-red-600',
+    'ring-amber-500',
+    'ring-blue-500',
+    'ring-pink-500',
+    'ring-purple-500',
+    'ring-cyan-500',
+    'ring-red-500',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-safelist: [
-  'bg-amber-600',
-  'bg-blue-600',
-  'bg-pink-600',
-  'bg-purple-600',
-  'bg-cyan-600',
-  'bg-red-600',
-  'ring-amber-500',
-  'ring-blue-500',
-  'ring-pink-500',
-  'ring-purple-500',
-  'ring-cyan-500',
-  'ring-red-500',
-]
