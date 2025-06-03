09b4a9075b35af936e7f2a0699177edaa0a72e05
JamStackSwipe <eric@hochacool.com>
2025-06-03 03:53:19 -0500

    Update genreList.js

--- src/utils/genreList.js
+++ src/utils/genreList.js
@@ -1,16 +1,37 @@
 // src/utils/genreList.js
-export const genreOptions = [
-  'pop',
-  'rock',
-  'hiphop',
-  'country',
-  'worship',
-  'lofi',
-  'electronic',
-  'comedy',
-  'ambient',
-  'indie',
-  'instrumental',
-  'spokenword',
-  'other',
-];
+
+export const genreFlavorMap = {
+  country: {
+    color: 'amber-500',
+    label: 'Country',
+    emoji: '🤠',
+  },
+  christian: {
+    color: 'blue-400',
+    label: 'Christian',
+    emoji: '🙏',
+  },
+  rock: {
+    color: 'red-500',
+    label: 'Rock',
+    emoji: '🎸',
+  },
+  hiphop: {
+    color: 'green-500',
+    label: 'Hip-Hop',
+    emoji: '🎤',
+  },
+  pop: {
+    color: 'pink-400',
+    label: 'Pop',
+    emoji: '💃',
+  },
+  acoustic: {
+    color: 'yellow-400',
+    label: 'Acoustic',
+    emoji: '🎻',
+  },
+};
+
+// Optional: use this if you want to list genres in a dropdown
+export const genreOptions = Object.keys(genreFlavorMap);
