502c2a708c68480bc5646a3c502a146ae9a09689
JamStackSwipe <eric@hochacool.com>
2025-10-22 03:10:12 -0500

    Create genreList.js

---
+++ utils/genreList.js
@@ -0,0 +1,27 @@
+export const genreFlavorMap = {
+  country_roots: {
+    label: 'Country & Roots 🤠',
+    color: 'amber',
+  },
+  hiphop_flow: {
+    label: 'Hip-Hop & Flow 🎤',
+    color: 'purple',
+  },
+  rock_raw: {
+    label: 'Rock & Raw 🤘',
+    color: 'red',
+  },
+  pop_shine: {
+    label: 'Pop & Shine ✨',
+    color: 'pink',
+  },
+  spiritual_soul: {
+    label: 'Spiritual & Soul ✝️',
+    color: 'blue',
+  },
+  
+  comedy_other: {
+  label: 'Comedy & Other 😂',
+  color: "green",
+  },
+};
