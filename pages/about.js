25152c54700cd48bf6e7ec02d31ae72d462de319
JamStackSwipe <eric@hochacool.com>
2025-10-22 03:37:59 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
-import About from '../src/screens/AboutScreen'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import About from '../screens/AboutScreen'
 
 export default function AboutPage() {
   return (
