d6a39b7f5d77be5b003680a99853d79bf619c3ef
JamStackSwipe <eric@hochacool.com>
2025-10-21 21:55:34 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../src/index.css'
+import { AuthProvider } from '../src/components/AuthProvider'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
