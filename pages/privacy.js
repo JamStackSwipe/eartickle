e9b0da2f6c99af2d64c2976ecd8f5473ebf737fe
JamStackSwipe <eric@hochacool.com>
2025-10-22 03:39:57 -0500

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,6 +1,6 @@
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
-import Privacy from '../src/screens/Privacy'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Privacy from '../screens/Privacy'
 
 export default function PrivacyPage() {
   return (
