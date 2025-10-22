de366bc0a7b8e69fbb2f0947d6762bc807fbd81a
JamStackSwipe <eric@hochacool.com>
2025-10-22 03:40:13 -0500

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,6 @@
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
-import ProfileScreen from '../src/screens/ProfileScreen'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ProfileScreen from '../screens/ProfileScreen'
 
 export default function Profile() {
   return (
