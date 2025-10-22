0362f318a93bdd2b2fd0fd1544f590158c53a4e4
JamStackSwipe <eric@hochacool.com>
2025-10-21 21:52:13 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 
 const Footer = () => (
   <footer className="w-full bg-black text-gray-400 text-xs py-4 px-4 text-center mt-auto">
     <div className="space-x-3">
-      <Link to="/privacy" className="hover:underline">🔒 Privacy</Link>
-      <Link to="/terms" className="hover:underline">📄 Terms</Link>
-      <Link to="/about" className="hover:underline">📖 About</Link>
-      <Link to="/genres" className="hover:underline">🎨 Genres</Link>
+      <Link href="/privacy" className="hover:underline">🔒 Privacy</Link>
+      <Link href="/terms" className="hover:underline">📄 Terms</Link>
+      <Link href="/about" className="hover:underline">📖 About</Link>
+      <Link href="/flavors" className="hover:underline">🎨 Flavors</Link>
       <a href="https://github.com/JamStackSwipe/eartickle/wiki" target="_blank" rel="noreferrer" className="hover:underline">📘 Wiki</a>
       <a href="https://github.com/JamStackSwipe/eartickle" target="_blank" rel="noreferrer" className="hover:underline">🔗 GitHub</a>
     </div>
