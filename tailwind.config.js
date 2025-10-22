0005662243e49a2aa79302f3d036ba78ad0121e5
JamStackSwipe <eric@hochacool.com>
2025-10-21 20:54:00 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", 
+    "./pages/**/*.{js,jsx,ts,tsx}"
+  ],
   safelist: [
     'bg-amber-600',
     'bg-blue-600',
