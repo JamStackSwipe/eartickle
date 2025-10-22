337a2c86e9ae2cf958aeed7cd5557fce52416dff
JamStackSwipe <eric@hochacool.com>
2025-10-22 03:38:17 -0500

    Update charts.js

--- pages/charts.js
+++ pages/charts.js
@@ -1,6 +1,6 @@
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
-import ChartsScreen from '../src/screens/ChartsScreen'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ChartsScreen from '../screens/ChartsScreen'
 
 export default function Charts() {
   return (
